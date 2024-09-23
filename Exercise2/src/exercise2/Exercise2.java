ae91a35f9344e818bfc52b17875772d167e90a09
am.lozano <am.lozano@106-18.info2>
2024-09-23 13:52:28 +0200

    Exercise 2 terminado

---
+++ Exercise2/src/exercise2/Exercise2.java
@@ -0,0 +1,10 @@
+package exercise2;
+
+public class Exercise2 {
+	public static void main(String[] args) {
+		System.out.println("Hello, how are you? ");
+		System.out.println("Fine thanks.");
+		// println te lo pone un una línea a parte
+	}
+
+}
